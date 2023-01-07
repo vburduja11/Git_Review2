59293acddead87b2e07c34f95e5f64b504bc3503
Vadim Burduja <114306435+vburduja11@users.noreply.github.com>
2023-01-07 13:32:51 -0600

    Update FirstClass.java

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -4,5 +4,7 @@ public class FirstClass {
 
     public static void main(String[] args) {
         System.out.println("First class file");
+        //some changes
+        System.out.println("First class file");
     }
 }
